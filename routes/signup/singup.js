d40301c60d86a0ea60e3b5cbd717fbb281251ff4
gavand-sagar <gavand.sagar.s@gmail.com>
2023-03-30 22:08:54 +0530

    userCreate updated

--- routes/signup/singup.js
+++ routes/signup/singup.js
@@ -6,14 +6,14 @@ const signupRoutes = Router();
 // its not s simple post req,
 // its has a file with name 'avatar'
 signupRoutes.post('/',
-    upload.single('avatar')
-    , (req, res) => {
+    // upload.single('avatar'), 
+    (req, res) => {
 
         // the new filename that get created for you from mongodb
-        let filename = req.file?.filename
+        // let filename = req.file?.filename
         
         let obj = req.body;
-        obj['avatar'] = filename
+        // obj['avatar'] = filename
         
         createItem('users', obj).then(x => {
             return res.json("created")
