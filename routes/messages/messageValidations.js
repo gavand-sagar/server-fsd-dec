55324afe81e646d1bf73e19fb9f763c45056cbb9
gavand-sagar <gavand.sagar.s@gmail.com>
2023-04-08 09:57:11 +0530

    typeing

--- routes/messages/messageValidations.js
+++ routes/messages/messageValidations.js
@@ -3,3 +3,6 @@ import { Validations } from "../../validator.js";
 export const mesageValidations = {
     content: [Validations.required]
 }
+export const typingValidations = {
+    typing: [Validations.required]
+}
--- routes/messages/messages.js
+++ routes/messages/messages.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createItem, getAllItems } from "../../mongo-db-utillities.js";
 import { validateBody } from "../../validator.js";
-import { mesageValidations } from "./messageValidations.js";
+import { mesageValidations, typingValidations } from "./messageValidations.js";
 import wssEvents from "../../web-socket-events.js";
 
 const messageRoutes = Router();
@@ -12,12 +12,22 @@ messageRoutes.post('',
         const msgObj = req.body;
         msgObj['author'] = req.headers.userObject.username;
         msgObj['time'] = new Date();
+        msgObj['status'] = 'sent'
         createItem('messages', msgObj)
             .then(x => {
                 msgObj['_id'] = x.insertedId;
-                wssEvents.emit('send-message',msgObj)
+                wssEvents.emit('send-message', msgObj)
                 res.json(msgObj)
             })
+    })
+
+messageRoutes.put('/typing',
+    validateBody(typingValidations),
+    (req, res) => {
+        const msgObj = req.body;
+        msgObj['author'] = req.headers.userObject.username;
+        wssEvents.emit('send-message', msgObj)
+        res.json(msgObj)
     })
 
 //get all songs from database
